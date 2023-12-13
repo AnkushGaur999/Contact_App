9e741b7d2792235ded4daaee3006b849b164dc24
ankushgaur <ankugaur999@gmail.com>
2023-12-13 22:34:02 +0530

    Room Database with MVVM

---
+++ app/src/main/java/com/example/roomdbwithmvvm/ui/activities/AddContactActivity.kt
@@ -0,0 +1,12 @@
+package com.example.roomdbwithmvvm.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.roomdbwithmvvm.R
+
+class AddContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+    }
+}
---
+++ app/src/main/java/com/example/roomdbwithmvvm/ui/activities/ContactInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.roomdbwithmvvm.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.roomdbwithmvvm.R
+
+class ContactInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_info)
+    }
+}
